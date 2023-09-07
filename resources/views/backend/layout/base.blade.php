246ef299f924f2b323f8902c4a709cdbed0cfe66
Dani lukman <danilukman2206@gmail.com>
2023-09-07 22:16:59 +0700

    asd

--- resources/views/backend/layout/base.blade.php
+++ resources/views/backend/layout/base.blade.php
@@ -19,9 +19,9 @@
     <!-- Bootstrap Css -->
     <link href="{{ asset('assets/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
     <!-- Icons Css -->
-    <link href="{{ asset('assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/css/icons.min.css') }}?v={{rand(000,999)}}" rel="stylesheet" type="text/css" />
     <!-- App Css-->
-    <link href="{{ asset('assets/css/app.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/css/app.min.css') }}?v={{rand(000,999)}}" id="app-style" rel="stylesheet" type="text/css" />
 
     <!-- DataTables -->
     <link href="{{ asset('assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css') }}" rel="stylesheet"
--- resources/views/backend/layout/navbar.blade.php
+++ resources/views/backend/layout/navbar.blade.php
@@ -190,7 +190,7 @@
              <div class="dropdown d-inline-block">
                  <button type="button" class="btn header-item waves-effect" id="page-header-user-dropdown"
                      data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                     <img class="rounded-circle header-profile-user" src="{{ asset('assets/images/users/user-4.jpg')}}"
+                     <img class="rounded-circle header-profile-user" src="{{ asset('') }}storage/images/users/{{ request()->user()->image }}"
                          alt="Header Avatar">
                  </button>
                  <div class="dropdown-menu dropdown-menu-end">
